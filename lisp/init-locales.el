3e66382b6cd94d2bbac5bc64bdceac4b062f1d9f
yujun <yujun1@xiaomi.com>
2015-11-27 22:57:35 +0800

    Add methods of copy and kill current string.

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -22,13 +22,19 @@
 ;;; Golbal key bindings
 (global-set-key (kbd "C-M-_") 'undo-tree-redo)
 (global-set-key (kbd "M-?") 'comment-or-uncomment-region-or-line)
+(global-set-key (kbd "M-w") 'copy-selection-or-current-string)
+(global-set-key (kbd "C-M-w") 'kill-current-string)
+(global-set-key (kbd "M-k") 'copy-current-line)
 (global-set-key (kbd "C-M-j") 'next-line-beginning-and-newline-and-indent)
 (global-set-key (kbd "C-j") 'previous-line-end-and-newline-and-indent)
 (global-set-key (kbd "C-a") 'back-to-indentation-or-beginning)
 (global-set-key (kbd "M-,") 'pop-tag-mark)
-(global-set-key (kbd "C-x t") (lambda ()
-                                (interactive)
-                                (ansi-term "/bin/zsh")))
+(global-set-key (kbd "C-c C-s a") (lambda ()
+                                    (interactive)
+                                    (ansi-term "/bin/zsh")))
+(global-set-key (kbd "C-c C-s e") (lambda ()
+                                    (interactive)
+                                    (eshell)))
 
 (global-set-key (kbd "C-c C-p l") 'package-list-packages)
 (global-set-key (kbd "C-c C-p i") 'package-install)
@@ -59,12 +65,16 @@
 (set-face-background 'linum "#1c1c1c")
 (setq linum-format "%3d ")
 
+;;; Paren match
+(custom-set-faces
+ '(show-paren-match ((t (:background "#fd971f" :foreground "#fff")))))
+
 ;;; Display time
-(setq display-time-24hr-format t
-      display-time-day-and-date t
-      display-time-use-mail-icon t
-      display-time-interval 10
-      display-time-format "%H:%M %A %m月%d日")
+ (setq display-time-24hr-format t
+       display-time-day-and-date t
+       display-time-use-mail-icon t
+       display-time-interval 10
+       display-time-format "%H:%M %A %m月%d日")
 (display-time-mode 1)
 
 :; General function
@@ -76,6 +86,48 @@
         (setq beg (region-beginning) end (region-end))
       (setq beg (line-beginning-position) end (line-end-position)))
     (comment-or-uncomment-region beg end)))
+
+(defun begin-and-end-point-cons-of-current-string ()
+  (let ((cur (point))
+        beg
+        end)
+    (cond ((region-active-p)
+           (setq beg (region-beginning) end (region-end)))
+          ((string-match-p "[0-9-a-zA-Z_]" (word-at-point))
+           (progn
+             (setq beg (+ (re-search-backward "[^0-9-a-zA-Z_]") 1))
+             (forward-char 1)
+             (setq end (- (re-search-forward "[^0-9-a-zA-Z_]") 1))
+             (backward-char 1)))
+          ;;; Spliters
+          (t
+           (setq beg cur end cur)))
+    (progn
+      (goto-char cur)
+      (cons beg end))))
+
+(defun copy-selection-or-current-string ()
+  (interactive)
+  (if (region-active-p)
+      (setq beg (region-beginning) end (region-end))
+    (setq beg (car (begin-and-end-point-cons-of-current-string)) end (cdr (begin-and-end-point-cons-of-current-string))))
+  (copy-region-as-kill beg end))
+
+(defun kill-current-string ()
+  (interactive)
+  (kill-region (car (begin-and-end-point-cons-of-current-string)) (cdr (begin-and-end-point-cons-of-current-string))))
+
+(defun copy-current-line ()
+  (interactive)
+  (let ((cur (point))
+        beg
+        end)
+    (back-to-indentation)
+    (setq beg (point))
+    (move-end-of-line 1)
+    (setq end (point))
+    (copy-region-as-kill beg end)
+    (goto-char cur)))
 
 (defun previous-line-end-and-newline-and-indent ()
   "Move curve to previous line end then newline and indent."

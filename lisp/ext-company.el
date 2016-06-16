6c55fa5a0269a58a0577504a4de738bd1e874a9b
yujun <yujun1@xiaomi.com>
2016-06-16 15:32:37 +0800

    Refactor company complete.

--- lisp/ext-company.el
+++ lisp/ext-company.el
@@ -4,6 +4,47 @@
 
 (require 'company)
 
+;; (require-package 'company-flx)
+;; (with-eval-after-load 'company
+;;   (company-flx-mode +1))
+
+;;; Never company auto
+(setq company-idle-delay nil)
+
+;;; Key bindings
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+
+(add-to-list 'completion-at-point-functions 'synelics/ido-company 'append)
+
+(defun synelics/ido-company ()
+  "Select `company-complete' candidates by `ido'."
+  (interactive)
+
+  ;;; Start complete
+  (let (
+        ;; Since company will auto complete common part
+        (complete-start-pos (point))
+        candidate-prefix candidates)
+
+    ;;; Company first
+    (unless company-candidates
+      (company-complete))
+
+    ;;; Then record states, and quit default frontend
+    (when company-point
+      (setq candidate-prefix company-prefix)
+      (setq candidates company-candidates)
+      (company-abort))
+
+    ;;; Show as ido
+    (when candidates
+      (let ((pos (point)))
+        (insert (ido-completing-read "Compeletion: " candidates nil nil candidate-prefix))
+        (if (> (length candidate-prefix) 0)
+            (delete-region (- complete-start-pos (length candidate-prefix)) pos))))
+    ))
+
 ;;; Custom company theme
 (deftheme molokai-overrides)
 
@@ -29,64 +70,6 @@
    ((t :inherit font-lock-constant-face)))
 
  '(font-lock-comment-face ((t (:foreground "#888888" :slant italic)))))
-
-;;; Never company auto
-(setq company-idle-delay nil)
-
-;;; Key bindings
-(define-key company-active-map (kbd "C-s") 'company-select-next)
-(define-key company-active-map (kbd "C-r") 'company-select-previous)
-(define-key company-mode-map (kbd "TAB") 'synelics/tab-indent-or-complete)
-
-(defun synelics/tab-indent-or-complete ()
-  "Smart tab, to indent, yas or complete."
-  (interactive)
-  (defun check-expansion ()
-    (save-excursion
-      (if (looking-at "\\_>") t
-        (backward-char 1)
-        (if (looking-at "\\.") t
-          (backward-char 1)
-          (if (looking-at "->") t nil)))))
-
-  (defun do-yas-expand ()
-    (let ((yas/fallback-behavior 'return-nil))
-      (yas/expand)))
-
-  (if (minibufferp)
-      (minibuffer-complete)
-    (if (or (not yas/minor-mode)
-            (null (do-yas-expand)))
-        (if (check-expansion)
-            (synelics/ido-company)
-          (indent-for-tab-command)))))
-
-(defun synelics/ido-company ()
-  "Select `company-complete' candidates by `ido'."
-  (interactive)
-
-  ;;; Start complete
-  (let (
-        ;; Since company will auto complete common part
-        (complete-start-pos (point))
-        candidate-prefix candidates)
-
-    ;;; Company first
-    (unless company-candidates
-      (company-complete))
-
-    ;;; Then record states, and quit default frontend
-    (when company-point
-      (setq candidate-prefix company-prefix)
-      (setq candidates company-candidates)
-      (company-abort))
-
-    ;;; Show as ido
-    (when candidates
-      (let ((pos (point)))
-        (insert (ido-completing-read "Compeletion: " candidates))
-        (delete-region (- complete-start-pos (length candidate-prefix)) pos)))))
-
 
 ;;; Bug: pos-tip-hide error
 (require 'company-quickhelp)
